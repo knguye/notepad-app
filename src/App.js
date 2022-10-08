a73b909f9d40212e5be64f92347d7225760661b1
kcacademy <info@kevscodingacademy.com>
2022-10-07 23:35:29 -0400

    Added "AddButton" to set up prompt

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
+  renderAddPrompt(){
+    return (
+      <AddPrompt
+        
+      />
+    )
+  }
+
   render () {
     return (
     <div className="App">
       <div className="App-header">
         <h1>Notes App</h1>
-        <button>+</button>
+        <AddButton/>
       </div>
       <Board>
         <Note color="red" title="Note 1"/>
@@ -24,6 +32,9 @@ class Board extends React.Component {
   constructor(props){
     super(props);
     this.name = "Notepad"
+    this.state = {
+      paused: false
+    }
   }
 
   render() {
@@ -38,18 +49,50 @@ class Board extends React.Component {
 class Note extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      height: "50vh"
+    }
   }
 
-  resizeNote(){
-    
+  // Unused, TODO: Use to change height based on textarea line breaks
+  resizeNote(setHeight){
+    this.setState = {
+      height: setHeight
+    }
   }
 
   render() {
     return (
-    <div className={"Note Note-" + this.props.color} style={{ backgroundColor: this.props.color }} >
+    <div className={"Note Note-" + this.props.color} style={{ backgroundColor: this.props.color, height: this.state.height }} >
       <h2>{this.props.title}</h2>
       <textarea></textarea>
     </div>
+    );
+  }
+}
+
+function AddPrompt(){
+  return (
+    <div className="AddPrompt"></div>
+  )
+}
+
+class AddButton extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  openPrompt(){
+    Board.setState = {
+      paused: true
+    }
+
+    return <AddPrompt/>
+  }
+
+  render(){
+    return (
+      <button className="AddButton" onClick={this.openPrompt}>+</button>
     );
   }
 }
